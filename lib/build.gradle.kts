c4c50d755850c754071757be9581f071ff8284e8
Aleks Rūtiņš <aleks@rutins.com>
2024-01-27 12:12:42 -0500

    Make it all public

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     `maven-publish`
 }
 
-version = "0.1.4"
+version = "0.1.5"
 group = "com.farthergate"
 
 publishing {
--- lib/src/main/java/com/farthergate/ctrlcurve/PIDCorrection.java
+++ lib/src/main/java/com/farthergate/ctrlcurve/PIDCorrection.java
@@ -1,5 +1,5 @@
 package com.farthergate.ctrlcurve;
 
-interface PIDCorrection {
-    double apply(PIDScope scope, double initial, double current, double target, double correction);
+public interface PIDCorrection {
+    public double apply(PIDScope scope, double initial, double current, double target, double correction);
 }
